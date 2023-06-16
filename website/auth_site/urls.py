4e0c9264effeb4fcd517841496c1cd2b5b8f3782
Bjj <a.urmanow@mail.ru>
2023-06-16 23:26:57 +0700

    add html views

--- website/auth_site/urls.py
+++ website/auth_site/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
    path('', views.index, name='auth_site'),
    path('about', views.about, name='about'),
+   path('create', views.create, name='create'),
     #path('about/', views.index(), name='about-club'),
 ]
--- website/auth_site/views.py
+++ website/auth_site/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Task
-
-from django.shortcuts import render
+from .forms import TaskForm
+from django.shortcuts import render, redirect
 def index(request):
     tasks = Task.objects.order_by('id')
     return render(request, 'auth_site/index.html', {'title': 'main site', 'tasks': tasks})
@@ -9,3 +9,19 @@ def index(request):
 def about(request):
     return render(request, 'auth_site/about.html')
 
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            error = 'Форма была не верной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'auth_site/create.html')
